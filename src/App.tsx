04efcca71394eeaf3842563fb0e2d6877763820f
tysonOliveira <thaisonoliveira@gmail.com>
2022-06-22 22:16:51 -0300

    added event route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
 import { client } from "./lib/apollo"
-import { Event} from './pages/Event'
+import { Router } from "./Router"
 
 function App() {
-  
-
   return (
-   <div>
-    <Event />
-   </div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
