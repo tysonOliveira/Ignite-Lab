b9b6b863b4a6d55f7957d0cce8af2a38c3d0e8b7
tysonOliveira <thaisonoliveira@gmail.com>
2022-06-24 12:50:01 -0300

    src assets png

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore',
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all',
+  },
+}
 
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oabadz1l0h01xxhhlwckdl/master',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
 })
