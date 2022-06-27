566803af5ca88f9f6fc2930fa5c9d877ab8ca459
tysonOliveira <thaisonoliveira@gmail.com>
2022-06-27 20:46:45 -0300

    home page responsive

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -27,10 +27,10 @@ export function Subscribe() {
   }
   //bg-blur
   return (
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center ">
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       
-      <div className="w-full lg:max-w-[1100px] block lg:flex items-center justify-between mt-20 mx-auto">
-        <div className="pr-1 text-center lg:text-left lg:block flex flex-col items-center lg:max-w-[640px]">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto lg:flex-col">
+        <div className="pr-1 max-w-[640px] lg:max-w-[900px] lg:flex lg:flex-col lg:px-8 lg:items-center lg:text-center lg:gap-5">
           <Logo />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
@@ -41,7 +41,7 @@ export function Subscribe() {
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="lg:mt-10 p-8 bg-gray-700 border border-gray-500 rounded lg:w-full">
           <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
@@ -69,12 +69,17 @@ export function Subscribe() {
         </div>
       </div>
       
-      <div className="bg-code  bg-no-repeat bg-contain md:bg-center bg-top w-[98%] h-48 md:h-[590px]"></div>
-
       
-      <div className="gap-2 bg-black w-full h-36 md:h-20 flex flex-col items-center justify-center md:flex-row md:justify-between px-5 text-gray-300">
+      <div className="bg-code  bg-no-repeat bg-contain bg-center  w-[98%] h-[590px] md:h-[300px]"></div>
+      
+      
+      <div className=" bg-black w-full h-20 flex items-center justify-between px-5 text-gray-300 md:flex-col md:h-32 md:justify-around md:text-center">
+        
+        <div className="flex items-center gap-6 md:flex-col md:gap-4">
+
         <LogoRocketseat />
         <span className="">Rocketseat - Todos os direitos reservados </span> 
+        </div>
         <p>
           Políticas de privacidade
         </p> 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'pt': {'max': '1280px'},
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
       },
       backgroundImage: {
         blur: 'url(/src/assets/blur-background.png)',
